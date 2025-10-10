a7124b16659c065f6ee1f80692326199cbf178a9
Jheff-conceicao <conceicao.jeferson@gmail.com>
2025-10-10 09:33:13 -0300

    continuação

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 
+
+import Nome from './components/Nome';
 
 function App() {
+  const [ aluno, setAluno] = useState("Sujeito Programador");
+
+  function handleChangeName(nome) {
+    setAluno(nome);
+  }
   return (
     <div>
-      <h1>Bem vindo ao meu projeto!</h1>
+      <h1>Componente App</h1> <br />
+      <h2>Olá: {aluno} </h2>
+      <button onClick={ () => handleChangeName('Jeferson da conceicao') }>
+        Mudar nome do aluno
+      </button>
+     
+    
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/components/Nome.js
@@ -0,0 +1,7 @@
+function Nome({aluno, idade}) {
+  return (
+    <span>Bem-vindo: Jeferson {aluno} - Idade: {idade} anos</span>
+  )
+}
+
+export default Nome;
